6ad06d4dde269dd213499faa820f37d3c5542812
jiangbaiyan <jiangbaiyan@jd.com>
2019-12-04 18:06:24 +0800

    修复父组件无法接收子组件值的bug

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,10 +1,29 @@
 <template>
-    <NosLogin :dialog-visible="true"></NosLogin>
+    <NosLogin :params="params" @login-finish="handleLoginFinish"></NosLogin>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        methods: {
+            handleLoginFinish(data) {
+                // 登录成功，跳到首页
+                if (data.status === 200) {
+                    this.$router.push('/');
+                } else { // 登录失败，刷新页面
+                    this.$alert('账号或密码错误');
+                }
+            }
+        },
+        data() {
+            return {
+                params: {
+                    appId: 'uc_all',
+                    accessToken: 'uc_all',
+                    timestamp: 131231
+                }
+            }
+        }
     }
 </script>
 
