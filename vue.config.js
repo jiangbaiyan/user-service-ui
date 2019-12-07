311ce13c1946e5eb4490abf825e1da6ea27a89b6
jiangbaiyan <987082332@qq.com>
2019-12-07 12:02:57 +0800

    部署

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
     devServer: {
         proxy: 'http://152.136.125.67:9600/v1/'
     },
-    outputDir: 'dist',
-    assetsDir: './',
     runtimeCompiler: undefined,
     productionSourceMap: false,
     parallel: undefined,
