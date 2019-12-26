ffd78ab39bea42e07d0f5ed5723a94033409639e
jiangbaiyan <jiangbaiyan@jd.com>
2019-12-26 09:25:30 +0800

    临时

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,12 @@ const routes = [
         path: '/auth',
         name: 'auth',
         component: () => import('../views/Auth/List')
+    },
+    // 临时
+    {
+        path: '/present',
+        name: 'present',
+        component: () => import('../views/Present')
     }
 ];
 
