77923476d769889a6b4de89cd4ac4933b9c63808
jiangbaiyan <987082332@qq.com>
2019-12-26 22:16:09 +0800

    update

--- src/views/Present.vue
+++ src/views/Present.vue
@@ -1,8 +1,9 @@
 <template>
-    <audio src="1.mp3" controls style="display:block; margin:20% auto;"></audio>
+    <audio id="audio" src="20191226_214816.mp3" controls style="display:block; margin:20% auto;"></audio>
 </template>
 
 <script>
+    document.getElementById('audio').volume = 1;
     export default {
         name: "Present"
     }
